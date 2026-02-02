18e6101589f277e78a1758c64953f8a7dcb02ad4
PK-1304 <pushkark1304@gmail.com>
2026-02-02 22:43:32 +0530

    Generate Path page

---
+++ client/src/pages/GeneratePath.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GeneratePath = () => {
+  return (
+    <div className='text-black'>
+        Generate Path page
+    </div>
+  )
+}
+
+export default GeneratePath
