976444319ac777e390ee911e8554f179ffaae6e7
PK-1304 <pushkark1304@gmail.com>
2026-02-04 10:07:35 +0530

    add protect and public routes

--- client/src/router.jsx
+++ client/src/router.jsx
@@ -1,4 +1,6 @@
 import {createBrowserRouter} from "react-router-dom"
+import PublicRoute from "./components/PublicRoute"
+import ProtectedRoute from "./components/ProtectedRoute"
 import Signup from "./pages/Signup"
 import Login from "./pages/Login"
 import GeneratePath from "./pages/GeneratePath"
@@ -6,15 +8,27 @@ import GeneratePath from "./pages/GeneratePath"
 const router = createBrowserRouter([
     {
         path: "/auth/signup",
-        element: <Signup />
+        element: (
+            <PublicRoute>
+                <Signup />
+            </PublicRoute>
+        )
     },
     {
         path: "/auth/login",
-        element: <Login />
+        element: (
+            <PublicRoute>
+                <Login />
+            </PublicRoute>
+        )
     },
     {
         path: "/",
-        element: <GeneratePath />
+        element: (
+            <ProtectedRoute>
+                <GeneratePath />
+            </ProtectedRoute>
+        )
     }
 ])
 
